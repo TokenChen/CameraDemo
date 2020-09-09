a17485c32db94346151723c5dfa231bc02db8302
chenzhentao <chenzhentao@baidu.com>
2020-09-09 10:40:19 +0800

    简化示例代码

--- app/src/main/java/com/example/ice/coursetable/CameraActivity.java
+++ app/src/main/java/com/example/ice/coursetable/CameraActivity.java
@@ -1,5 +1,13 @@
 package com.example.ice.coursetable;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Objects;
+
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
@@ -15,6 +23,7 @@ import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
 import android.hardware.camera2.CaptureRequest;
 import android.media.Image;
 import android.media.ImageReader;
@@ -27,35 +36,19 @@ import android.os.HandlerThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.SparseIntArray;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.ice.coursetable.flashstrategy.AutoStrategy;
-import com.example.ice.coursetable.flashstrategy.CloseStrategy;
-import com.example.ice.coursetable.flashstrategy.FlashStrategy;
-import com.example.ice.coursetable.flashstrategy.KeepOpenStrategy;
-import com.example.ice.coursetable.flashstrategy.OpenStrategy;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.nio.ByteBuffer;
-import java.util.Arrays;
-
-public class CameraActivity extends AppCompatActivity implements View.OnClickListener{
+public class CameraActivity extends AppCompatActivity {
     private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
     private static final String TAG = "CameraActivity";
 
@@ -66,40 +59,35 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         ORIENTATIONS.append(Surface.ROTATION_180, 270);
         ORIENTATIONS.append(Surface.ROTATION_270, 180);
     }
-    private View mFlashLayout;
-    private TextView tv_flashStatus, tv_flashClose, tv_flashOpen, tv_flashKeepOpen, tv_flashAuto;
-    private ImageView iv_thumb, iv_changeCamera, iv_flash;
+
     private SurfaceHolder mSurfaceHolder;
     private SurfaceView mSurfaceView;
-    private Handler childHandler;
+    private Handler subThreadHandler;
     private Handler mainHandler;
-    private int mCameraID;
+    private String mCameraID;
     private ImageReader mImageReader;
     private CameraDevice mCameraDevice;//摄像头设备
     private CameraManager mCameraManager;
     private CameraCaptureSession mCameraCaptureSession;
-    public final int FLASH_ON = 1;
-    public final int FLASH_OFF = 2;
     private CaptureRequest.Builder mPreviewRequestBuilder;
-
-    public final int BACK_CAMERA = 0; //后置摄像头的CameraId
-    public final int FRONT_CAMERA = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);//隐藏标题栏
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);//隐藏状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);//隐藏状态栏
         setContentView(R.layout.activity_camera);
 
         initView();
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == 1){
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == 1) {
             for (int grantResult : grantResults) {
-                if(grantResult != PackageManager.PERMISSION_GRANTED){
+                if (grantResult != PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(this, "对不起，没有权限，无法正常使用相机", Toast.LENGTH_SHORT).show();
                     return;
                 }
@@ -109,22 +97,7 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
     }
 
     private void initView() {
-        iv_flash = findViewById(R.id.iv_flash);
-        iv_thumb = findViewById(R.id.iv_thumb);
-        tv_flashStatus = findViewById(R.id.tv_flash_status);
         mSurfaceView = findViewById(R.id.surfaceView);
-        iv_changeCamera = findViewById(R.id.iv_change);
-
-        mFlashLayout = findViewById(R.id.layout_flash_text);
-        tv_flashAuto = mFlashLayout.findViewById(R.id.tv_flash_auto);
-        tv_flashClose = mFlashLayout.findViewById(R.id.tv_flash_close);
-        tv_flashKeepOpen = mFlashLayout.findViewById(R.id.tv_flash_keep_open);
-        tv_flashOpen =mFlashLayout.findViewById(R.id.tv_flash_open);
-
-        tv_flashAuto.setOnClickListener(this);
-        tv_flashClose.setOnClickListener(this);
-        tv_flashKeepOpen.setOnClickListener(this);
-        tv_flashOpen.setOnClickListener(this);
 
         mSurfaceHolder = mSurfaceView.getHolder();
         mSurfaceHolder.setKeepScreenOn(true);
@@ -135,14 +108,17 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
                 // 初始化Camera
                 //获取摄像头权限
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if(checkSelfPermission(Manifest.permission.CAMERA)!=PackageManager.PERMISSION_GRANTED
-                            ||checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
-                        requestPermissions(new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+                    if (checkSelfPermission(Manifest.permission.CAMERA)
+                            != PackageManager.PERMISSION_GRANTED
+                            || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(new String[] {Manifest.permission.CAMERA,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
                         findViewById(R.id.btn_control).setClickable(false);
-                    }else {
+                    } else {
                         initCamera2();
                     }
-                }else{
+                } else {
                     initCamera2();
                 }
             }
@@ -163,97 +139,87 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
 
         findViewById(R.id.btn_control).setOnClickListener(v -> takePicture());
 
-        //点击闪光灯图标
-        iv_flash.setOnClickListener(v -> {
-            Animation anim = AnimationUtils.loadAnimation(this, R.anim.flash_in);
-            mFlashLayout.setAnimation(anim);
-            mFlashLayout.setVisibility(View.VISIBLE);
-        });
-
-        //查看照片
-        iv_thumb.setOnClickListener(v -> {
-            File file=new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)+"/temp.jpg");
-            Intent it =new Intent(Intent.ACTION_VIEW);
-            Uri mUri = Uri.parse("file://"+file.getPath());
-            it.setDataAndType(mUri, "image/*");
-            startActivity(it);
-        });
-
-        //更换摄像头
-        iv_changeCamera.setOnClickListener(v->changeCamera());
-
     }
 
     @SuppressLint({"MissingPermission", "NewApi"})
-    private void changeCamera(){
-        int a = 1;
+    private void changeCamera() {
+        try {
+            //先关闭之前的摄像头
+            if (mCameraDevice != null) {
+                mCameraDevice.close();
+                mCameraDevice = null;
+            }
+            if (mCameraID.equals(CameraMetadata.LENS_FACING_FRONT)) {
+                mCameraID = String.valueOf(CameraMetadata.LENS_FACING_BACK);
+            } else {
+                mCameraID = String.valueOf(CameraMetadata.LENS_FACING_FRONT);
+            }
+            mCameraManager.openCamera(mCameraID ,stateCallback, mainHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
     }
-//        try {
-//            //先关闭之前的摄像头
-//            if(mCameraDevice!=null) {
-//                mCameraDevice.close();
-//                mCameraDevice = null;
-//            }
-//            mCameraID^=1;
-//            mCameraManager.openCamera(mCameraID+"", stateCallback, mainHandler);
-//        } catch (CameraAccessException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
 
     @SuppressLint("NewApi")
     private void initCamera2() {
         findViewById(R.id.btn_control).setClickable(true);
 
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
         HandlerThread handlerThread = new HandlerThread("Camera2");
         handlerThread.start();
-        childHandler = new Handler(handlerThread.getLooper());
+        subThreadHandler = new Handler(handlerThread.getLooper());
         mainHandler = new Handler(getMainLooper());
 
-//        mCameraID = BACK_CAMERA;//后摄像头
-        mCameraID = BACK_CAMERA;//后摄像头
-        Log.d(TAG, "initCamera2: "+ CameraCharacteristics.LENS_FACING_BACK);
-        mImageReader = ImageReader.newInstance(1080, 1920, ImageFormat.JPEG,1);
-        mImageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() { //可以在这里处理拍照得到的临时照片 例如，写入本地
-            @Override
-            public void onImageAvailable(ImageReader reader) {
-//                mCameraDevice.close();
-//                mSurfaceView.setVisibility(View.INVISIBLE);
-                // 拿到拍照照片数据
-                Image image = reader.acquireNextImage();
-                ByteBuffer buffer = image.getPlanes()[0].getBuffer();
-                byte[] bytes = new byte[buffer.remaining()];
-                buffer.get(bytes);//由缓冲区存入字节数组
-                //保存
-
-                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-
-                if (bitmap != null) {
-                    if(mCameraID == FRONT_CAMERA){
-                        //前置摄像头拍的要先旋转180度
-                        bitmap = adjustPhotoRotation(bitmap,180);
-                    }
-                    iv_thumb.setImageBitmap(bitmap);
-                    writeToFile(bitmap);
-                }
-
-                image.close();
-            }
-        }, mainHandler);
-        //获取摄像头管理
-        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-//        mCameraManager.openCamera(mCameraID+"", stateCallback, mainHandler);
-
-
-
         try {
-//            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-//                return;
-//            }
+            String[] cameraIds = mCameraManager.getCameraIdList();
+            for (int i = 0; i < cameraIds.length; i++) {
+                CameraCharacteristics cameraCharacteristics =
+                        mCameraManager.getCameraCharacteristics(cameraIds[i]);
+                if (cameraCharacteristics.get(CameraCharacteristics.LENS_FACING)
+                        == CameraMetadata.LENS_FACING_BACK) {
+                    mCameraID = cameraIds[i];
+                }
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
 
-            //打开摄像头
-            mCameraManager.openCamera(mCameraID+"", stateCallback, mainHandler);
+        Log.d(TAG, "initCamera2: " + CameraCharacteristics.LENS_FACING_BACK);
+        mImageReader = ImageReader.newInstance(1080, 1920, ImageFormat.JPEG, 1);
+        mImageReader.setOnImageAvailableListener(
+                new ImageReader.OnImageAvailableListener() { //可以在这里处理拍照得到的临时照片 例如，写入本地
+                    @Override
+                    public void onImageAvailable(ImageReader reader) {
+                        //                mCameraDevice.close();
+                        //                mSurfaceView.setVisibility(View.INVISIBLE);
+                        // 拿到拍照照片数据
+                        Image image = reader.acquireLatestImage();
+                        ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                        byte[] bytes = new byte[buffer.remaining()];
+                        buffer.get(bytes);//由缓冲区存入字节数组
+                        //保存
+
+                        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+
+                        if (bitmap != null) {
+                            if (Objects.equals(mCameraID, CameraMetadata.LENS_FACING_FRONT)) {
+                                //前置摄像头拍的要先旋转180度
+                                bitmap = adjustPhotoRotation(bitmap, 180);
+                            }
+                            writeToFile(bitmap);
+                        }
+
+                        image.close();
+                    }
+                }, mainHandler);
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                    != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[] {Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return;
+            }
+            mCameraManager.openCamera(mCameraID + "", stateCallback, mainHandler);
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
@@ -305,10 +271,11 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
                         // 自动对焦
                         mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
                         // 打开闪光灯
-                        mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.FLASH_MODE_OFF);
+                        mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE,
+                                CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
                         // 显示预览
                         CaptureRequest previewRequest = mPreviewRequestBuilder.build();
-                        mCameraCaptureSession.setRepeatingRequest(previewRequest, null, childHandler);
+                        mCameraCaptureSession.setRepeatingRequest(previewRequest, null, subThreadHandler);
                     } catch (CameraAccessException e) {
                         e.printStackTrace();
                     }
@@ -318,22 +285,7 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
                 public void onConfigureFailed(CameraCaptureSession cameraCaptureSession) {
                     Toast.makeText(CameraActivity.this, "配置失败", Toast.LENGTH_SHORT).show();
                 }
-            }, childHandler);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @SuppressLint("NewApi")
-    public void changeFlash(int type){
-        try {
-            CaptureRequest.Builder requestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            requestBuilder.addTarget(mSurfaceHolder.getSurface());
-            if(type == FLASH_ON)
-                requestBuilder.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
-            else if(type == FLASH_OFF)
-                requestBuilder.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.FLASH_MODE_OFF);
-            mCameraCaptureSession.setRepeatingRequest(requestBuilder.build(),null,childHandler);
+            }, subThreadHandler);
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
@@ -342,6 +294,7 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
     /**
      * 摄像头创建监听
      */
+    @SuppressLint("NewApi")
     private CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
         @TargetApi(Build.VERSION_CODES.LOLLIPOP)
         @Override
@@ -387,59 +340,11 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
             captureRequestBuilder.set(CaptureRequest.JPEG_ORIENTATION, ORIENTATIONS.get(rotation));
             //拍照
             CaptureRequest mCaptureRequest = captureRequestBuilder.build();
-            mCameraCaptureSession.capture(mCaptureRequest, null, childHandler);
+            mCameraCaptureSession.capture(mCaptureRequest, null, subThreadHandler);
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
     }
-
-    @Override
-    public void onClick(View v) {
-        FlashStrategy strategy = null;
-        switch (v.getId()){
-            case R.id.tv_flash_close:
-                strategy = new CloseStrategy();
-                tv_flashStatus.setText("关闭");
-                break;
-            case R.id.tv_flash_auto:
-                tv_flashStatus.setText("自动");
-                strategy = new AutoStrategy();
-                break;
-            case R.id.tv_flash_open:
-                tv_flashStatus.setText("打开");
-                strategy = new OpenStrategy();
-                break;
-            case R.id.tv_flash_keep_open:
-                tv_flashStatus.setText("常亮");
-                strategy = new KeepOpenStrategy();
-                break;
-        }
-        //说明点击了文字 切换闪光灯
-        if(strategy!=null) {
-            //重新设置背景颜色
-            clearAllFlashTextBackground(v.getId());
-            //消失动画
-            closeFlashLayout();
-            //重新设置闪光灯
-            strategy.setCaptureRequest(mPreviewRequestBuilder, mCameraCaptureSession, childHandler);
-        }
-
-    }
-
-    public void closeFlashLayout(){
-        Animation anim = AnimationUtils.loadAnimation(this, R.anim.flash_out);
-        mFlashLayout.setAnimation(anim);
-        mFlashLayout.setVisibility(View.GONE);
-    }
-
-    public void clearAllFlashTextBackground(int id){
-        tv_flashClose.setBackground(null);
-        tv_flashOpen.setBackground(null);
-        tv_flashKeepOpen.setBackground(null);
-        tv_flashAuto.setBackground(null);
-        findViewById(id).setBackground(getResources().getDrawable(R.drawable.flash_text_shape));
-    }
-
 
 
 }
